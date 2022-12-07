1350396d1b3606ca9a1c01289cdd2f8519869435
Dmitry Kuklin <dmi-kuklin@yandex.ru>
2022-12-07 23:39:45 +0300

    Home tasks 25

---
+++ Task25/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число A:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+var result = Math.Pow(a,b);
+Console.WriteLine("Результат = {0}", result);
