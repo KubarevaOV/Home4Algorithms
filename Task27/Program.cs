e4f82e5f75d026c082f72ece9096703a7d23487f
Dmitry Kuklin <dmi-kuklin@yandex.ru>
2022-12-07 23:45:32 +0300

    Home tasks 27

---
+++ Task27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+var sum = 0;
+
+do {
+    var number = a % 10;
+    sum = sum + number;
+
+    a = a / 10;
+} while (a > 1);
+
+Console.WriteLine("Сумма цифр в числе равна: {0}", sum);
