fba05a53a8981c9bbecebcb53aad82812386fb80
Dmitry Kuklin <dmi-kuklin@yandex.ru>
2022-12-08 00:20:12 +0300

    Home tasks 29

---
+++ Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[8];
+
+for (int i = 0; i < 8; i++) {
+    Console.WriteLine("Введите число:");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int j = 0; j < array.Length; j++) {
+    if (j == 0) {
+        Console.Write("[{0}, ", array[j]);
+    }else if (j == array.Length - 1) {
+        Console.WriteLine("{0}]", array[j]);
+    } else {
+        Console.Write("{0}, ", array[j]);
+    }
+}
